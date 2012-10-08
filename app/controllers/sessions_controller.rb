0abbb1cb6454fe2d97eeea5dcd966d178e2fd8d2
mitya777 <dfabrikant@matchbox.net>
2012-10-08 17:08:10 -0400

    redirect to techniques after signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       render 'new'
     else
       sign_in user
-      redirect_back_or user
+      redirect_back_or techniques_path 
     end
   end
 
